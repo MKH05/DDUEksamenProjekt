0473f27915e62de8782632c809feccac4d5c5cca
MKH05 <mattias@haugvik.dk>
2025-04-11 09:45:32 +0200

    fix

--- scripts/factory_area.gd
+++ scripts/factory_area.gd
@@ -1,13 +1,11 @@
 extends StaticBody2D
 
-var player_in_area: bool = false
-
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.has_method("player"):
-		player_in_area = true
+		Globals.player_in_area = true
 		print("Player entered the area")
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body.has_method("player"):
-		player_in_area = false
+		Globals.player_in_area = false
 		print("Player exited the area")
--- scripts/globals.gd
+++ scripts/globals.gd
@@ -1,4 +1,9 @@
 #Player
 var player_max_health: int = 100
 var player_health: int = 100
+
+var player_max_o2: int = 1000
+var player_o2: int = 1000
+
 var player_in_area: bool = false
+ 
